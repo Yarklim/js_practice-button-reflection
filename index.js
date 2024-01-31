29f230b3db190568a16ed6f21149e4520d163508
Yaroslav Klimas <yarklim@gmail.com>
2024-01-31 13:58:47 +0200

    created index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const constraints = {
+  audio: false,
+  video: true,
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(function (stream) {
+    const video = document.getElementById('camera');
+    video.srcObject = stream;
+    video.onloadedmetadata = function (e) {
+      video.play();
+    };
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
